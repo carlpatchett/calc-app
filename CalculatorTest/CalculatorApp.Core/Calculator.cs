71248ec2448231ccb7b60e0608bfa61a03c2d6bc
Carl Patchett <carl.patchett@hotmail.co.uk>
2022-06-26 10:21:11 +0100

    Initial commit containing CalculatorTest solution, ISimpleCalculator interface and basic implementation.

---
+++ CalculatorTest/CalculatorApp.Core/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using CalculatorApp.Core.Interfaces;
+
+namespace CalculatorApp.Core
+{
+    /// <summary>
+    /// A calculator capable of basic arithmetic.
+    /// </summary>
+    public class Calculator : ISimpleCalculator
+    {
+        /// <summary>
+        /// Adds two numbers together.
+        /// </summary>
+        /// <param name="x">The first number.</param>
+        /// <param name="y">The second number</param>
+        /// <returns>The result of adding both numbers together.</returns>
+        public int Add(int x, int y)
+        {
+            var result = x + y;
+            return result;
+        }
+
+        /// <summary>
+        /// Subtracts two numbers from eachother.
+        /// </summary>
+        /// <param name="x">The first number.</param>
+        /// <param name="y">The second number.</param>
+        /// <returns>The result of subtracting the first number from the second.</returns>
+        public int Subtract(int x, int y)
+        {
+            var result = x - y;
+            return result;
+        }
+
+        /// <summary>
+        /// Multiplies two numbers by eachother.
+        /// </summary>
+        /// <param name="x">The first number.</param>
+        /// <param name="y">The second number.</param>
+        /// <returns>The result of multiplying the first number by the second number.</returns>
+        public int Multiply(int x, int y)
+        {
+            var result = x * y;
+            return result;
+        }
+
+        /// <summary>
+        /// Divides two numbers by eachother.
+        /// </summary>
+        /// <param name="x">The first number.</param>
+        /// <param name="y">The second number.</param>
+        /// <returns>The result of dividing the first number by the second number.</returns>
+        public float Divide(int x, int y)
+        {
+            var result = x / y;
+            return result;
+        }
+    }
+}
---
+++ CalculatorTest/CalculatorApp.Core/Interfaces/ISimpleCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorApp.Core.Interfaces
+{
+    public interface ISimpleCalculator
+    {
+        int Add(int start, int amount);
+        int Subtract(int start, int amount);
+        int Multiply(int start, int amount);
+        float Divide(int start, int by);
+    }
+}
